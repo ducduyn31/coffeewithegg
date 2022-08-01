999217206195409f2054d6b3924022996581ff4d
ducduyn31 <ducduy.ababat@gmail.com>
2022-08-02 02:34:17 +0700

    [CWE-10] Update Dockerfile for adam

--- apps/dashboard/src/environments/environment.prod.ts
+++ apps/dashboard/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendServer: '',
+  backendServer: 'https://adam.coffeewithegg.com/graphql',
   opsEntry: 'https://ops.coffeewithegg.com',
   sunnyStreamEntry: 'https://sunnystream.coffeewithegg.com/remoteEntry.js',
 }
