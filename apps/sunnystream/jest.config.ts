0a97bcd91b52a90ed8398b27816d579750c8f807
ducduyn31 <ducduy.ababat@gmail.com>
2022-08-18 01:28:00 +0700

    [CWE-5] Update Tests

--- apps/sunnystream/jest.config.ts
+++ apps/sunnystream/jest.config.ts
@@ -3,8 +3,8 @@ export default {
   displayName: 'sunnystream',
   preset: '../../jest.preset.js',
   transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.(js|jsx|ts|tsx|css|json)?$': 'ts-jest',
+    '^(?!.*\\.svg$)': '@nrwl/react/plugins/jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/sunnystream',
