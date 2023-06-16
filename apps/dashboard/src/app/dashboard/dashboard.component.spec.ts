bd839cf1e1bfe9936716d4d23deb3038517a0032
ducduyn31 <ducduy.ababat@gmail.com>
2023-06-17 07:25:25 +1000

    CWE-18 Update tests

--- apps/dashboard/src/app/dashboard/dashboard.component.spec.ts
+++ apps/dashboard/src/app/dashboard/dashboard.component.spec.ts
@@ -12,6 +12,9 @@ import { mockProject } from '../mocks'
 import { CommonAngularModule } from '@coffeewithegg/common-angular'
 import { ProjectOverviewComponent } from '../project-overview/project-overview.component'
 import { RouterTestingModule } from '@angular/router/testing'
+import { HeaderComponent } from '../header/header.component'
+import { ProfileComponent } from '../profile/profile.component'
+import { ContactComponent } from '../contact/contact.component'
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent
@@ -29,7 +32,13 @@ describe('DashboardComponent', () => {
       getAllProjectsMappings: mockedGetAllProjects,
     }
     await TestBed.configureTestingModule({
-      declarations: [DashboardComponent, ProjectOverviewComponent],
+      declarations: [
+        DashboardComponent,
+        ProjectOverviewComponent,
+        HeaderComponent,
+        ProfileComponent,
+        ContactComponent,
+      ],
       imports: [
         CommonAngularModule,
         ApolloTestingModule,
@@ -65,8 +74,12 @@ describe('DashboardComponent', () => {
     expect(projects).toHaveLength(1)
   })
 
-  it('should have an about section', async () => {
-    const about = fixture.nativeElement.querySelector('coffeewithegg-about')
-    expect(about).toBeTruthy()
+  it('should have a header and a contact', async () => {
+    const header = fixture.nativeElement.querySelector('coffeewithegg-header')
+    const profile = fixture.nativeElement.querySelector('coffeewithegg-profile')
+    const contact = fixture.nativeElement.querySelector('coffeewithegg-contact')
+    expect(header).toBeTruthy()
+    expect(profile).toBeTruthy()
+    expect(contact).toBeTruthy()
   })
 })
