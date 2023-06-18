5821907d6afba9381b00b5cc706c0b74d2f2183d
ducduyn31 <ducduy.ababat@gmail.com>
2023-06-19 03:15:50 +1000

    CWE-19 Update tests

--- apps/ops/src/app/login/login.component.spec.ts
+++ apps/ops/src/app/login/login.component.spec.ts
@@ -1,13 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { LoginComponent } from './login.component'
+import { of } from 'rxjs'
+import { AuthService } from '@auth0/auth0-angular'
 
 describe('LoginComponent', () => {
   let component: LoginComponent
   let fixture: ComponentFixture<LoginComponent>
+  const mockedAuthService = {
+    loginWithRedirect: jest.fn(),
+    isAuthenticated$: of(true),
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockedAuthService,
+        },
+      ],
     }).compileComponents()
 
     fixture = TestBed.createComponent(LoginComponent)
